b224cd7456197dc98fab717b941e431d23b5d697
hanqing <229075284@qq.com>
2023-04-16 14:31:30 +0800

    update

--- server/config/env.ts
+++ server/config/env.ts
@@ -2,10 +2,11 @@
 let REMOTE_FILEPATH_PREFIX
 let REMOTE_UPLOAD_URL
 if (process.env.NODE_ENV === 'production') {
-  REMOTE_FILEPATH_PREFIX = 'http://back.freehan.tk:9001'
-  REMOTE_UPLOAD_URL = 'http://back.freehan.tk:9000/upload'
+  REMOTE_FILEPATH_PREFIX = 'http://back.freehan.ml:9001'
+  REMOTE_UPLOAD_URL = 'http://back.freehan.ml:9000/upload'
 } else {
-  REMOTE_FILEPATH_PREFIX = 'http://localhost:9001'
+  REMOTE_FILEPATH_PREFIX = 'http://back.freehan.ml:9001'
+  // REMOTE_FILEPATH_PREFIX = 'http://localhost:9001'
   REMOTE_UPLOAD_URL = 'http://localhost:9000/upload'
 }
 export { REMOTE_FILEPATH_PREFIX, REMOTE_UPLOAD_URL }
